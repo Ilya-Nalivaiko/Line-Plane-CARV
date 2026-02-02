ce1ac26d2560b273710535a203088a9384d63478
Ilya-Nalivaiko <65912861+Ilya-Nalivaiko@users.noreply.github.com>
2026-02-02 00:53:56 -0700

    run

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# probably adjust dev video0
+docker run \
+  -p 5901:5901 \
+  -p 8080:8080 \
+  --tmpfs /tmp:rw,size=100m \
+  --tmpfs /root/.vnc \
+  --rm \
+  --privileged \
+  -e DISPLAY=:$DISPLAY \
+  -e QT_X11_NO_MITSHM=1 \
+  --security-opt seccomp=unconfined \
+  --security-opt apparmor=unconfined \
+  --device=/dev/video0:/dev/video0 \
+  -v /temp/.X11-unix \
+  --network host \
+  -it imjunaida/line_plane_carv-docker:v1.0 /bin/bash
