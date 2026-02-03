5312a996ca1a0028a6b3d42a48fb0cd237f57616
Ilya-Nalivaiko <65912861+Ilya-Nalivaiko@users.noreply.github.com>
2026-02-03 16:19:17 -0700

    asan

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd Thirdparty/DBoW2
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wall -Wextra -pedantic"
-make -j$nproc
+make -j8
 
 cd ../../g2o
 
@@ -13,7 +13,7 @@ echo "Configuring and building Thirdparty/g2o ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wall -Wextra -pedantic"
-make -j$nproc
+make -j8
 
 
 cd ../../../
@@ -29,7 +29,7 @@ echo "Configuring and building ORB_SLAM2 ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wall -Wextra -pedantic"
-make -j$nproc
+make -j8
 
 #cd ..
 #echo "Converting vocabulary to binary"
